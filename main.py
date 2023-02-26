287f264525f3063fcb41737eb9ed83345ea91013
Ezequiel Capaceta <ezequielcapaceta@zeki.local>
2023-02-26 11:39:26 -0800

    added questions

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ def flatten_and_sort(array):
         for i in item:
             arr.append(i)
     return sorted(arr)
+
+"""
+1. Make sure to answer the following questions about your coding process:
+
+2. How does this solution ensure data immutability?
+
+3. Is this solution a pure function? Why or why not?
+
+4. Is this solution a higher order function? Why or why not?
+
+5. Would it have been easier to solve this problem using a different programming style?
+Why in particular is functional programming a helpful paradigm when solving this problem?
+"""
